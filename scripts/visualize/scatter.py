e06c8873b0c6fa5fdce69ffd57368bf9ed1913c8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2023-04-12 07:41:41 +0530

    Fix scatter_pca_2

--- scripts/visualize/scatter.py
+++ scripts/visualize/scatter.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
+import numpy as np
 
 spark = SparkSession.builder \
     .appName("Random Forest Regressor") \
@@ -9,17 +10,25 @@ spark = SparkSession.builder \
 # Load data
 df = spark.read.parquet(f"../../tmp/datasets/pca_2")
 
-# Split the pca_features column into separate columns
-df = df.select(df['pca_features'].getItem(0).alias('x'), df['pca_features'].getItem(1).alias('y'))
-
 # Convert Spark DataFrame to Pandas DataFrame
-pdf = df.toPandas()
+df = df.toPandas()
 
-print(pdf.head())
+# Calculate the mean of the 'frp' column
+mean_frp = np.mean(df['frp'])
+
+# Create two classes based on the mean of the 'frp' column
+df['class'] = np.where(df['frp'] < mean_frp, 1, 2)
 
 # Plot data in a scatter plot
-plt.scatter(pdf['x'], pdf['y'])
-plt.xlabel('Column 1')
-plt.ylabel('Column 2')
-plt.title('Scatter Plot')
+colors = {1: 'red', 2: 'blue'}
+plt.scatter(df['pca_0'], df['pca_1'], c=df['class'].apply(lambda x: colors[x]), marker='.')
+plt.xlabel('pca_0')
+plt.ylabel('pca_1')
+plt.title('Visualization of data after PCA')
 plt.show()
+
+# Save the plot as a high-resolution image
+plt.savefig('../../tmp/scatter_pca_2.png', dpi=300)
+
+# Stop the Spark session
+spark.stop()
